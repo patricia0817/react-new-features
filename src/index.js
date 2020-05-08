36c0ea38c3645079cc00034ba2eabfaf7d0ea3d2
amihut <amihut@EN610723>
2020-05-08 14:50:21 +0300

    set up project with react-create-app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div>My new content</div>,
+  document.getElementById( 'root' )
 );
 
 // If you want your app to work offline and load faster, you can change
