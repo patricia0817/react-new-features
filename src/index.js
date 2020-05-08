23e2eb99b708b711e01c936b93a06d5b2b00f105
amihut <amihut@EN610723>
2020-05-08 16:16:40 +0300

    use useState function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <div>My new content</div>,
-  document.getElementById( 'root' )
-);
+
+const App = ( props ) => {
+  const [ count, setCount ] = useState( props.count );
+  const [ text, setText ] = useState( '' );
+
+  return (
+    <div>
+      <p>The current { text || 'count' } is { count }</p>
+      <button onClick={ () => { setCount( count + 1 ) } }>+1</button>
+      <button onClick={ () => { setCount( props.count ) } }>Reset</button>
+      <button onClick={ () => { setCount( count - 1 ) } }>-1</button>
+      <input value={ text } onChange={ ( e ) => { setText( e.target.value ) } } />
+    </div>
+  )
+}
+
+App.defaultProps = {
+  count: 0
+}
+
+ReactDOM.render( <App count={ 2 } />, document.getElementById( 'root' ) );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
