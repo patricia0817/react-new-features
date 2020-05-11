7176cbfdad1e0da3ceb7c405010bea6e1f779f93
amihut <amihut@EN610723>
2020-05-11 15:23:46 +0300

    break app in components

---
+++ src/components/NoteApp.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useReducer } from 'react';
+
+import notesReducer from '../reducers/notes'
+import NoteList from './NoteList';
+import AddNoteForm from './AddNoteForm';
+
+const NoteApp = () => {
+  const [ notes, dispatch ] = useReducer( notesReducer, [] );
+
+  useEffect( () => {
+    const notes = JSON.parse( window.localStorage.getItem( 'notes' ) )
+    if ( notes ) {
+      dispatch( { type: 'POPULATE_NOTES', notes } )
+    }
+  }, [] )
+
+  useEffect( () => {
+    window.localStorage.setItem( 'notes', JSON.stringify( notes ) )
+  }, [ notes ] )
+
+  const removeNote = ( title ) => {
+    dispatch( { type: 'REMOVE_NOTE', title } )
+  }
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <NoteList notes={ notes } removeNote={ removeNote } />
+      <AddNoteForm dispatch={ dispatch } />
+    </div>
+  )
+}
+
+export { NoteApp as default }
--- src/index.js
+++ src/index.js
@@ -1,85 +1,8 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-
-const notesReducer = ( state, action ) => {
-  switch ( action.type ) {
-    case 'ADD_NOTE':
-      return [
-        ...state,
-        { title: action.title, body: action.body }
-      ]
-    case 'POPULATE_NOTES':
-      return action.notes
-    case 'REMOVE_NOTE':
-      return state.filter( ( note ) => ( note.title !== action.title ) )
-    default:
-      return state
-  }
-}
-
-const NoteApp = () => {
-  const [ notes, dispatch ] = useReducer( notesReducer, [] );
-  const [ title, setTitle ] = useState( '' );
-  const [ body, setBody ] = useState( '' );
-
-  useEffect( () => {
-    const notes = JSON.parse( window.localStorage.getItem( 'notes' ) )
-    if ( notes ) {
-      dispatch( { type: 'POPULATE_NOTES', notes } )
-    }
-  }, [] )
-
-  useEffect( () => {
-    window.localStorage.setItem( 'notes', JSON.stringify( notes ) )
-  }, [ notes ] )
-
-  const addNote = ( e ) => {
-    e.preventDefault();
-    dispatch( { type: 'ADD_NOTE', title, body } );
-    setTitle( '' );
-    setBody( '' );
-  }
-
-  const removeNote = ( title ) => {
-    dispatch( { type: 'REMOVE_NOTE', title } )
-  }
-
-  return (
-    <div>
-      <h1>Notes</h1>
-      { notes.map( ( note ) => (
-        <Note key={ note.title } note={ note } removeNote={ removeNote } />
-      ) ) }
-      <p>Add note</p>
-      <form onSubmit={ addNote }>
-        <input value={ title } onChange={ ( e ) => setTitle( e.target.value ) } />
-        <textarea onChange={ ( e ) => setBody( e.target.value ) } placeholder='Add note body' value={ body }></textarea>
-        <button>Add note</button>
-      </form>
-    </div>
-  )
-}
-
-const Note = ( { note, removeNote } ) => {
-  useEffect( () => {
-    console.log( 'setting up effect!' );
-
-    return () => {
-      console.log( 'Cleaning up effect!' )
-    }
-  }, [] )
-
-  return (
-    <div>
-      <h3>{ note.title }</h3>
-      <p>{ note.body }</p>
-      <button onClick={ () => removeNote( note.title ) }>X</button>
-    </div>
-  )
-
-}
+import NoteApp from './components/NoteApp';
 
 
 ReactDOM.render( <NoteApp />, document.getElementById( 'root' ) );
